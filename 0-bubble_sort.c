d16fa6c462ab671c045b978da0036f19fa3b1897
Yara Alrasheed <11982@holbertonstudents.com>
2025-10-26 18:49:36 +0300

    Implement Bubble sort algorithm
    Implemented the Bubble sort algorithm to sort an array of integers in ascending order. Added error handling for null array and size less than 2.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,1 +1,34 @@
+#include "sort.h"
 
+/**
+* bubble_sort - sorts an array of integers in ascending order using the Bubble sort algorithm.
+*
+* @array: the array to sort
+* @size: the size of the array
+*/
+
+void bubble_sort(int *array, size_t size) {
+
+size_t i, n = size;
+int temp;
+int swapped;
+
+if (array == NULL || size < 2)
+	return;
+
+do {
+	swapped = 0;
+	for (i = 0; i < n - 1; i++)
+	{
+	if (array[i] > array[i + 1])
+	temp = array[i];
+	array[i] = array[i + 1];
+	array[i + 1] = temp;
+
+	print_array(array, size);
+	swapped = 1;
+}
+}
+	n--;
+} while (swapped);
+}
