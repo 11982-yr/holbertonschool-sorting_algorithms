f141f5c6806b1c4831c67edd476fa212a7b22771
root <11982@holbertonstudents.com>
2025-10-26 16:20:19 +0000

    Fix bubble sort implementation

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,33 +1,35 @@
 #include "sort.h"
 
 /**
-* bubble_sort - sorts an array of integers in ascending order using the Bubble sort algorithm.
+* bubble_sort - sorts an array in ascending order using Bubble sort.
 *
 * @array: the array to sort
 * @size: the size of the array
 */
 
-void bubble_sort(int *array, size_t size) {
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, n = size;
+	int temp;
+	int swapped;
 
-size_t i, n = size;
-int temp;
-int swapped;
-
-if (array == NULL || size < 2)
+	if (array == NULL || size < 2)
 	return;
 
-do {
+	do {
 	swapped = 0;
+
 	for (i = 0; i < n - 1; i++)
 	{
 	if (array[i] > array[i + 1])
+	{
 	temp = array[i];
 	array[i] = array[i + 1];
 	array[i + 1] = temp;
 
 	print_array(array, size);
 	swapped = 1;
-}
+	}
 }
 	n--;
 } while (swapped);
