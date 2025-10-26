425fd939df0705f4d4d0f5fae3e5da43ba1fb598
11968-ship-it <11968@holbertonstudents.com>
2025-10-26 19:31:36 +0300

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -18,7 +18,7 @@ size_t i, j, min_idx;
 for (i = 0; i < size - 1; i++)
 {
 min_idx = i;
-for (j = i +1; j < size; j++)
+for (j = i + 1; j < size; j++)
 {
 if (array[j] < array[min_idx])
 min_idx = j;
