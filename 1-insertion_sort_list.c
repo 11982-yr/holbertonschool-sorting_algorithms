9b5d91362cf837386972eca626a60954ebb84394
root <11982@holbertonstudents.com>
2025-10-27 15:12:29 +0000

    Implement insertion sort for doubly linked list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,26 +2,31 @@
 
 /**
 * swap_nodes - helper for swapping nodes.
+*
+* @list: list of nodes
+* @left: left swap
+* @right: right swap
 */
 
 static void swap_nodes(listint_t **list, listint_t *left, listint_t *right)
 {
-  if (left->prev)
-    left->prev->next = right;
-else
-    *list = right;
-  
-  if (right->next)
-    right->next->prev = left;
+	if (left->prev)
+	left->prev->next = right;
+	else
+	*list = right;
 
-  right->prev = left->prev;
-  left->prev = right;
-  left->next = right->next;
-  right->next = left;
+	if (right->next)
+	right->next->prev = left;
+
+	right->prev = left->prev;
+	left->prev = right;
+	left->next = right->next;
+	right->next = left;
 }
 
 /**
-* insertion_sort_list - sorts a doubly linked list in ascending order using the Insertion sort
+* insertion_sort_list - sorts doubly linked list in ascending order
+* using Insertion sort
 *
 * @list: list of integers.
 */
@@ -33,15 +38,15 @@ void insertion_sort_list(listint_t **list)
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 	return;
 
-  current = (*list)->next;
+	current = (*list)->next;
 	while (current)
 {
-  insert = current;
-  while (insert->prev && insert->prev->n > insert->n)
-    {
-    swap_nodes(list, insert->prev, insert);
-  print_list(*list);
-    }
+	insert = current;
+	while (insert->prev && insert->prev->n > insert->n)
+	{
+	swap_nodes(list, insert->prev, insert);
+	print_list(*list);
+	}
 current = insert->next;
 }
 }
