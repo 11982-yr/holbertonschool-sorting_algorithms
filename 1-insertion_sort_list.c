ab6373efbb4bdf21e258096210604293abd87e7d
Yara Alrasheed <11982@holbertonstudents.com>
2025-10-27 18:03:07 +0300

    Add insertion sort implementation for linked list
    Implemented insertion sort for a doubly linked list with a helper function to swap nodes.

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,1 +1,47 @@
+#include "sort.h"
 
+/**
+* swap_nodes - helper for swapping nodes.
+*/
+
+static void swap_nodes(listint_t **list, listint_t *left, listint_t *right)
+{
+  if (left->prev)
+    left->prev->next = right;
+else
+    *list = right;
+  
+  if (right->next)
+    right->next->prev = left;
+
+  right->prev = left->prev;
+  left->prev = right;
+  left->next = right->next;
+  right->next = left;
+}
+
+/**
+* insertion_sort_list - sorts a doubly linked list in ascending order using the Insertion sort
+*
+* @list: list of integers.
+*/
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *insert;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	return;
+
+  current = (*list)->next;
+	while (current)
+{
+  insert = current;
+  while (insert->prev && insert->prev->n > insert->n)
+    {
+    swap_nodes(list, insert->prev, insert);
+  print_list(*list);
+    }
+current = insert->next;
+}
+}
