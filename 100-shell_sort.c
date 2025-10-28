800eede941392316e8245dffdd789603e17aec13
root <11982@holbertonstudents.com>
2025-10-28 15:43:01 +0000

    Implement shell sort function using Knuth sequenceand add prototypes to header files

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,1 +1,42 @@
+#include "sort.h"
 
+/**
+* shell_sort = sorts an array in ascending order 
+* using the Shell sort algorithm and the Knuth sequence.
+*
+* @array: array of integers.
+* @size: the size of the array.
+*/
+
+void shell_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+
+ 	size_t gap = 1;
+
+	if (!array || size < 2)
+	return;
+
+	while (gap < size / 3)
+	gap = gap * 3 + 1;
+
+	while (gap > 0)
+	{
+    for (i = gap; i < size; i++)
+    {
+        temp = array[i];
+        j = i;
+
+        while (j >= gap && array[j - gap] > temp)
+        {
+            array[j] = array[j - gap];
+            j -= gap;
+        }
+
+        array[j] = temp;
+    }
+	print_array(array, size);
+	gap = (gap - 1) / 3;
+}
+}
