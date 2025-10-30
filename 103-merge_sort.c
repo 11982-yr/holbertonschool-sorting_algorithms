1c9f7550c588c1304f0889432f2dd0ef7fa30c07
Yara Alrasheed <11982@holbertonstudents.com>
2025-10-30 13:01:25 +0300

    Update 103-merge_sort.c

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,1 +1,19 @@
+#include "sort.h"
+#include <stdlib.h>
+
+/**
+* merge_sort - sorts an array in ascending order 
+* using the Merge sort algorithm.
+*
+* @array: array of integers.
+* @size: size of array.
+*/
+
+void merge_sort(int *array, size_t size)
+{
+  size_t n;
+  int k;
+
+  if (array == NULL || size < 2)
+    return;
 
